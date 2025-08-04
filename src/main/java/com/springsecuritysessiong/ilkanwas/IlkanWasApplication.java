fdda5e89ce20c1cb63b29c3cdde012918d7a2b41
nahyun <nahyun040411@naver.com>
2025-08-04 22:23:58 +0900

    test

---
+++ src/main/java/com/springsecuritysessiong/ilkanwas/IlkanWasApplication.java
@@ -0,0 +1,13 @@
+package com.springsecuritysessiong.ilkanwas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlkanWasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlkanWasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springsecuritysessiong/ilkanwas/IlkanWasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecuritysessiong.ilkanwas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlkanWasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
