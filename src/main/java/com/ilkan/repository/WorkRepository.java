4a353b84c098f0fc444b0caf33dd38dc4e932889
곽동욱 <kdw381@naver.com>
2025-08-08 23:44:26 +0900

    일거리 관련기능 repository 책임 재분리

--- src/main/java/com/ilkan/repository/WorkRepository.java
+++ src/main/java/com/ilkan/repository/WorkRepository.java
@@ -1,7 +1,10 @@
 package com.ilkan.repository;
 
 import com.ilkan.domain.entity.Work;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WorkRepository extends JpaRepository<Work,Long> {
+public interface WorkRepository extends JpaRepository<Work, Long> {
+    Page<Work> findByRequesterId(Long requesterId, Pageable pageable);
 }
