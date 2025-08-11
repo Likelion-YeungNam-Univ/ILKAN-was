3910bf95729f706cb9242a2b3ec3411a5d510e4b
nahyun <nahyun040411@naver.com>
2025-08-12 02:28:27 +0900

    feat : 사용자 인증 기능 - 로그인 DTO 추가

---
+++ src/main/java/com/ilkan/dto/userdto/LoginRequestDto.java
@@ -0,0 +1,16 @@
+package com.ilkan.dto.userdto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@Schema(description = "역할 로그인 요청 DTO")
+public class LoginRequestDto {
+
+    @Schema(description = "사용자 역할", example = "REQUESTER")
+    private final String role;
+}
---
+++ src/main/java/com/ilkan/dto/userdto/LoginResponseDto.java
@@ -0,0 +1,17 @@
+package com.ilkan.dto.userdto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@Schema(description = "역할 로그인 응답 DTO")
+public class LoginResponseDto {
+
+    @Schema(description = "사용자 역할", example = "REQUESTER")
+    private final String role;
+
+    @Schema(description = "로그인 결과 메시지", example = "로그인 성공")
+    private final String message;
+}
