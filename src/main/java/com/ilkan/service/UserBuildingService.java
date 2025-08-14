878bf562ec409b53f617047ce5caaf95caa8e459
곽동욱 <kdw381@naver.com>
2025-08-14 17:01:49 +0900

    fix : 재사용성 높은 role -> userid 매핑 메소드 재분리

--- src/main/java/com/ilkan/service/UserBuildingService.java
+++ src/main/java/com/ilkan/service/UserBuildingService.java
@@ -5,6 +5,7 @@ import com.ilkan.domain.enums.ReservationStatus;
 import com.ilkan.dto.reservationdto.OwnerBuildingResDto;
 import com.ilkan.dto.reservationdto.UserBuildingResDto;
 import com.ilkan.repository.UserBuildingRepository;
+import com.ilkan.util.RoleMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,29 +19,19 @@ public class UserBuildingService {
 
     // 수행자가 사용중인 공간 조회
     public Page<UserBuildingResDto> findUsingBuildingsByPerformer(String role, Pageable pageable) {
-        Long performerId = getUserIdByRole(role);
+        Long performerId = RoleMapper.getUserIdByRole(role);
         Page<Reservation> reservations = userBuildingRepository.findByPerformerId_IdAndBuildingStatus(performerId, ReservationStatus.IN_USE, pageable);
         return reservations.map(UserBuildingResDto::fromEntity);
     }
 
     // 건물주가 등록한 건물 조회
     public Page<OwnerBuildingResDto> getRegisteredBuildings(String role, Pageable pageable) {
-        Long ownerId = getUserIdByRole(role);
+        Long ownerId = RoleMapper.getUserIdByRole(role);
         Page<Reservation> buildings = userBuildingRepository.findByBuildingId_Owner_IdAndBuildingStatus(
                 ownerId, ReservationStatus.REGISTERED, pageable
         );
         return buildings.map(OwnerBuildingResDto::fromEntity);
     }
 
-
-    // role → userId 매핑
-    private Long getUserIdByRole(String role) {
-        switch (role.toUpperCase()) {
-            case "REQUESTER": return 1L;
-            case "PERFORMER": return 2L;
-            case "OWNER": return 3L;
-            default: throw new IllegalArgumentException("Unknown role: " + role);
-        }
-    }
 }
 
