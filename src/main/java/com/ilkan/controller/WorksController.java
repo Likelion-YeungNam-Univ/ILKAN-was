0f0b41a7d49aa67d0149b48dc7d6cdc8f1d36f2b
곽동욱 <kdw381@naver.com>
2025-08-15 01:53:12 +0900

    feat : 일거리 상세조회 controller 구현

--- src/main/java/com/ilkan/controller/WorksController.java
+++ src/main/java/com/ilkan/controller/WorksController.java
@@ -1,5 +1,6 @@
 package com.ilkan.controller;
 
+import com.ilkan.dto.workdto.WorkDetailResDto;
 import com.ilkan.dto.workdto.WorkListResDto;
 import com.ilkan.service.WorkService;
 import lombok.RequiredArgsConstructor;
@@ -9,13 +10,14 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/works")
 @RequiredArgsConstructor
-public class WokrsController {
+public class WorksController {
 
     private final WorkService workService;
 
@@ -29,4 +31,14 @@ public class WokrsController {
         }
         return ResponseEntity.ok(worksList);
     }
+
+    // 일거리 상세 조회
+    @GetMapping("/{taskId}")
+    public ResponseEntity<WorkDetailResDto> getWorkDetail(@PathVariable Long taskId) {
+        WorkDetailResDto workDetail = workService.getWorkDetail(taskId);
+        return ResponseEntity.ok(workDetail);
+    }
+
+
+
 }
