74a05cb435cfae52e560bd3d5d2dd9f2e62c8ccc
곽동욱 <kdw381@naver.com>
2025-08-14 22:30:28 +0900

    feat : 모든 일거리 조회 repository 구현

--- src/main/java/com/ilkan/repository/WorkRepository.java
+++ src/main/java/com/ilkan/repository/WorkRepository.java
@@ -2,6 +2,7 @@ package com.ilkan.repository;
 
 import com.ilkan.domain.entity.Work;
 import com.ilkan.domain.enums.Status;
+import jakarta.annotation.Nonnull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,7 @@ public interface WorkRepository extends JpaRepository<Work, Long> {
     Page<Work> findByRequesterId(Long requesterId, Pageable pageable); // 내가(의뢰자) 등록한 일거리조회
 
     Page<Work> findByPerformerIdAndStatus(Long performerId, Status status, Pageable pageable);// 내가(수행자) 수행중인 일거리 조회 , 내가(수행자) 지원한 일거리 조회
+
+    @Nonnull
+    Page<Work> findAll(@Nonnull Pageable pageable); // 모든 일거리조회
 }
