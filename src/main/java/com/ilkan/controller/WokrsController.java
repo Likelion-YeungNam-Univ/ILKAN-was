1dfcfa32cc9c8e3bb0ee5b5d3c44211461192b71
곽동욱 <kdw381@naver.com>
2025-08-14 23:21:53 +0900

    feat : 모든 일거리 조회 controller 구현

---
+++ src/main/java/com/ilkan/controller/WokrsController.java
@@ -0,0 +1,32 @@
+package com.ilkan.controller;
+
+import com.ilkan.dto.workdto.WorkListResDto;
+import com.ilkan.service.WorkService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/works")
+@RequiredArgsConstructor
+public class WokrsController {
+
+    private final WorkService workService;
+
+    // 모든 일거리 조회
+    @GetMapping
+    public ResponseEntity<Page<WorkListResDto>> getWorkList(
+            @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<WorkListResDto> worksList = workService.getWorkList(pageable);
+        if (worksList.isEmpty()) {
+            return ResponseEntity.ok().body(Page.empty()); // 성공 및 데이터는 없음
+        }
+        return ResponseEntity.ok(worksList);
+    }
+}
