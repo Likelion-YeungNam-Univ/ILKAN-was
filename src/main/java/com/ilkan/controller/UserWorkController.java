06421743612f7f4087c15400eaf41f547df2848e
곽동욱 <kdw381@naver.com>
2025-08-14 19:49:43 +0900

    fix : 역할검증로직 제거 후 어노테이션으로 검증

--- src/main/java/com/ilkan/controller/UserWorkController.java
+++ src/main/java/com/ilkan/controller/UserWorkController.java
@@ -1,5 +1,7 @@
 package com.ilkan.controller;
 
+import com.ilkan.auth.AllowedRoles;
+import com.ilkan.domain.enums.Role;
 import com.ilkan.dto.workdto.WorkResponseDto;
 import com.ilkan.service.WorkService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +9,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,16 +19,11 @@ public class UserWorkController {
     private final WorkService workService;
 
     // 내가 등록한 일거리 조회 (의뢰자)
+    @AllowedRoles(Role.REQUESTER)
     @GetMapping("/upload")
     public ResponseEntity<Page<WorkResponseDto>> getMyUploadedWorks(
             @RequestHeader("X-Role") String roleHeader,
             @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
-
-        // 역할검증 로직
-        if (!"REQUESTER".equals(roleHeader)) {
-            // 권한이 없으므로 403 Forbidden 에러 반환
-            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-        }
 
         // 서비스 호출하여 DTO로 변환된 페이징 결과 조회
         Page<WorkResponseDto> worksDto = workService.getWorksByRequester(roleHeader, pageable);
@@ -38,16 +34,11 @@ public class UserWorkController {
     }
 
     // 내가 수행중인 일거리 조회 (수행자)
+    @AllowedRoles(Role.PERFORMER)
     @GetMapping("/doing")
     public ResponseEntity<Page<WorkResponseDto>> doingWorksByPerformer(
             @RequestHeader("X-Role") String roleHeader,
             @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
-
-        // 역할 검증 로직 추가
-        if (!"PERFORMER".equals(roleHeader)) {
-            // 권한이 없으므로 403 Forbidden 에러 반환
-            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-        }
 
         Page<WorkResponseDto> worksDto = workService.doingWorksByPerformer(roleHeader, pageable);
         if (worksDto.isEmpty()) { // 성공 및 데이터는 없음
@@ -57,16 +48,11 @@ public class UserWorkController {
     }
 
     // 내가 지원한 일거리 조회 (수행자)
+    @AllowedRoles(Role.PERFORMER)
     @GetMapping("/applied")
     public ResponseEntity<Page<WorkResponseDto>> getAppliedWorks(
             @RequestHeader("X-Role") String roleHeader,
             @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
-
-        // 역할 검증 로직 추가
-        if (!"PERFORMER".equals(roleHeader)) {
-            // 권한이 없으므로 403 Forbidden 에러 반환
-            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-        }
 
         Page<WorkResponseDto> appliedWorks = workService.getAppliedWorksByPerformer(roleHeader, pageable);
         if (appliedWorks.isEmpty()) { // 성공 및 데이터는 없음
