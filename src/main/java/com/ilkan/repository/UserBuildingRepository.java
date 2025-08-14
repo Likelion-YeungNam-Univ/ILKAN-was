a797ec03053c100082f5596ffae5352cd1d15e9f
nahyun <nahyun040411@naver.com>
2025-08-14 18:29:56 +0900

    feat : buildingStatus -> reservationStatus 변경 관련 수정

--- src/main/java/com/ilkan/repository/UserBuildingRepository.java
+++ src/main/java/com/ilkan/repository/UserBuildingRepository.java
@@ -1,11 +1,13 @@
 package com.ilkan.repository;
 
 import com.ilkan.domain.entity.Reservation;
-import com.ilkan.domain.enums.BuildingStatus;
+import com.ilkan.domain.enums.ReservationStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserBuildingRepository extends JpaRepository<Reservation, Long> {
-    Page<Reservation> findByPerformerIdAndBuildingStatus(Long performerId, BuildingStatus status, Pageable pageable);
+    Page<Reservation> findByPerformerId_IdAndReservationStatus(
+            Long performerId, ReservationStatus status, Pageable pageable);
+
 }
