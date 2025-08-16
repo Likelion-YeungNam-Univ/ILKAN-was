3054c9a4723744e817c5bed85a895c3d9aff64ae
곽동욱 <kdw381@naver.com>
2025-08-16 19:28:02 +0900

    fix : 와일드카드 import 제거 후 명확화

--- src/main/java/com/ilkan/controller/api/WorksApi.java
+++ src/main/java/com/ilkan/controller/api/WorksApi.java
@@ -17,7 +17,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Tag(name = "Works", description = "일거리 조회 및 관리 API")
 @RequestMapping(value = "/api/v1/works", produces = "application/json")
