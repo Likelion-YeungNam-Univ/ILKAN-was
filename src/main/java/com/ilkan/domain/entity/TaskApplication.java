2625caf7d4e589247adb7be4ac2f2b2a2b4b1fb3
곽동욱 <kdw381@naver.com>
2025-08-17 19:12:32 +0900

    feat : 일거리 지원내역 엔티티, 레포지토리 구현

---
+++ src/main/java/com/ilkan/domain/entity/TaskApplication.java
@@ -0,0 +1,45 @@
+package com.ilkan.domain.entity;
+
+import com.ilkan.domain.enums.Status;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDateTime;
+
+public class TaskApplication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id; // 지원기록 id
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id", nullable = false)
+    private long taskId; // 일거리 id
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "performer_id", nullable = false)
+    private long performer_id; // 수행자 id
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Status status;
+
+    @Column(name = "applied_at", nullable = false)
+    private LocalDateTime appliedAt;
+
+    @Column
+    private String message; // 자기소개
+
+    public void updateStatus(Status status) {this.status = status;}
+
+    public void updateAppliedAt(LocalDateTime appliedAt) {this.appliedAt = appliedAt;}
+
+    public void updateMessage(String message) {this.message = message;}
+}
