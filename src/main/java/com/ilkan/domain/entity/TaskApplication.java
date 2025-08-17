b124ac38482efa38919bf75f803be25ca8805f67
곽동욱 <kdw381@naver.com>
2025-08-17 19:41:24 +0900

    feat : 일거리 지원조회 엔티티 구현

--- src/main/java/com/ilkan/domain/entity/TaskApplication.java
+++ src/main/java/com/ilkan/domain/entity/TaskApplication.java
@@ -2,6 +2,7 @@ package com.ilkan.domain.entity;
 
 import com.ilkan.domain.enums.Status;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
@@ -10,22 +11,33 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class TaskApplication {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id; // 지원기록 id
+    @Column(name = "apply_id")
+    private Long id; // 지원기록 id
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "task_id", nullable = false)
-    private long taskId; // 일거리 id
+    private Work taskId; // 일거리 id
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "performer_id", nullable = false)
-    private long performer_id; // 수행자 id
+    private User performerId; // 수행자 id
 
     @Enumerated(EnumType.STRING)
     @Column
