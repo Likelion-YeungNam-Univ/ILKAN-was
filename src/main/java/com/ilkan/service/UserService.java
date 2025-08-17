2e3b8a3ee67b8e0ced7006e24988f6bbdd850ac1
nahyun <nahyun040411@naver.com>
2025-08-17 14:48:17 +0900

    docs : UserService단 주석 추가

--- src/main/java/com/ilkan/service/UserService.java
+++ src/main/java/com/ilkan/service/UserService.java
@@ -16,6 +16,15 @@
     public class UserService {
         private final UserRepository userRepository;
 
+        /**
+         * 주어진 {@link Role} 에 해당하는 사용자의 프로필을 조회합니다.
+         *
+         * @param role 프로필 조회 요청자 역할
+         * @return 사용자 프로필 응답 DTO
+         * @throws com.ilkan.exception.RoleExceptions.NotFound
+         *        해당 역할의 사용자가 존재하지 않는 경우
+         */
+
         @Transactional(readOnly = true)
         public UserProfileResDto getUserProfile(Role role) {
             User user = userRepository.findFirstByRole(role)
