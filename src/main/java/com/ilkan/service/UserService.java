a296ce26d10a85830baf3f360b08fc439b16e84e
nahyun <nahyun040411@naver.com>
2025-08-17 14:42:17 +0900

    feat : 프로필 조회 API UserService 구현

--- src/main/java/com/ilkan/service/UserService.java
+++ src/main/java/com/ilkan/service/UserService.java
@@ -1,12 +1,36 @@
-package com.ilkan.service;
+    package com.ilkan.service;
 
-import com.ilkan.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+    import com.ilkan.domain.entity.User;
+    import com.ilkan.domain.enums.Role;
+    import com.ilkan.dto.userdto.UserProfileResDto;
+    import com.ilkan.exception.RoleExceptions;
+    import com.ilkan.repository.UserRepository;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.stereotype.Service;
+    import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
-    private final UserRepository userRepository;
+    import java.util.Optional;
 
-}
+    @Service
+    @RequiredArgsConstructor
+    public class UserService {
+        private final UserRepository userRepository;
+
+        @Transactional(readOnly = true)
+        public UserProfileResDto getUserProfile(Role role) {
+            User user = userRepository.findFirstByRole(role)
+                    .orElseThrow(() -> new RoleExceptions.NotFound(role));
+
+            return UserProfileResDto.builder()
+                    .userId(user.getId())
+                    .name(user.getName())
+                    .phoneNumber(user.getPhoneNumber())
+                    .role(user.getRole().name())
+                    .profileImage(user.getProfileImage())
+                    .portfolioUrl(user.getPortfolioUrl())
+                    .organization(user.getOrganization())
+                    .build();
+
+        }
+
+    }
