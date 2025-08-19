a18acc2f925aac264437e90dab684c765781582b
nahyun <nahyun040411@naver.com>
2025-08-19 16:54:54 +0900

    feat : 빌딩 목록 리스팅 서비스 구현

--- src/main/java/com/ilkan/repository/BuildingRepository.java
+++ src/main/java/com/ilkan/repository/BuildingRepository.java
@@ -2,6 +2,9 @@ package com.ilkan.repository;
 
 import com.ilkan.domain.entity.Building;
 import jakarta.persistence.LockModeType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 
@@ -13,4 +16,7 @@ public interface BuildingRepository extends JpaRepository<Building, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select b from Building b where b.id = :id")
     Optional<Building> findForUpdate(@Param("id") Long id);
+
+    @EntityGraph(attributePaths = {"owner"})
+    Page<Building> findAll(Specification<Building> spec, Pageable pageable);
 }
