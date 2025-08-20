864ce40f61770bc66a0338f1b4ad20bd50afa00b
DongWook-Kwak <kdw381@naver.com>
2025-08-21 00:11:55 +0900

    feat : 공간대여 결제에 필요한 필드 추가

--- src/main/java/com/ilkan/domain/enums/BuildingStatus.java
+++ src/main/java/com/ilkan/domain/enums/BuildingStatus.java
@@ -2,6 +2,7 @@ package com.ilkan.domain.enums;
 
 public enum BuildingStatus {
     REGISTERED, // 등록된 (건물주 공간조회)
+    PAY_USEWAITING, // 결제 완료 및 사용대기
     APPLY_REVIEW, // 심사 신청
     IN_REVIEW, // 심사중
     END_REVIEW // 심사완료
