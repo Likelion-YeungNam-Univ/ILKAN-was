b366c6b7d6c89a7727846f5f3ef072babc3fc2a9
DongWook-Kwak <kdw381@naver.com>
2025-08-22 23:49:01 +0900

    fix : 의뢰자 등록작업조회 시 모집중, 배정됨 상태만 조회

--- src/main/java/com/ilkan/domain/work/repository/WorkRepository.java
+++ src/main/java/com/ilkan/domain/work/repository/WorkRepository.java
@@ -7,11 +7,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface WorkRepository extends JpaRepository<Work, Long> {
-    Page<Work> findByRequesterId(Long requesterId, Pageable pageable); // 내가(의뢰자) 등록한 일거리조회
+    Page<Work> findByRequesterIdAndStatusIn(Long requesterId, List<Status> statuses, Pageable pageable);// 내가(의뢰자) 등록한 일거리조회
 
     Page<Work> findByPerformer_IdAndStatusIn(Long performerId, Collection<Status> statuses, Pageable pageable); // 수행자 동시버튼
     Page<Work> findByRequester_IdAndStatusIn(Long requesterId, Collection<Status> statuses, Pageable pageable); // 의뢰자 동시버튼
@@ -23,4 +25,10 @@ public interface WorkRepository extends JpaRepository<Work, Long> {
     Optional<Work> findByIdAndRequesterId(Long taskId, Long requesterId); // 일거리 수정/삭제 시 권한체크용
 
     Page<Work> findByWorkCategory(WorkCategory category, Pageable pageable);
+
+    // 지금 시간 이전인데 아직 시작 안한 Work
+    List<Work> findByTaskStartBeforeAndStatus(LocalDateTime now, Status status);
+
+    // 지금 시간 이후인데 종료 안된 Work
+    List<Work> findByTaskEndBeforeAndStatus(LocalDateTime now, Status status);
 }
