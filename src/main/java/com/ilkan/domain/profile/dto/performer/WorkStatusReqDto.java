61cdf9c99c688dc8ca3306a819e45ec9417c7d16
DongWook-Kwak <kdw381@naver.com>
2025-08-22 23:42:16 +0900

    feat : 수행자 프로필 일거리 상태변경 dto 필드추가

--- src/main/java/com/ilkan/domain/profile/dto/performer/WorkStatusReqDto.java
+++ src/main/java/com/ilkan/domain/profile/dto/performer/WorkStatusReqDto.java
@@ -6,11 +6,21 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 @AllArgsConstructor
 @Schema(description = "수행자 프로필 일거리 상태 변경DTO")
 public class WorkStatusReqDto {
+
     @Schema(description = "일거리 상태", example = "IN_PROGRESS")
     private Status status;
+
+    @Schema(description = "작업 시작 시간", example = "2025-08-20T09:00:00")
+    private LocalDateTime taskStart;
+
+    @Schema(description = "작업 마감 시간", example = "2025-08-22T18:00:00")
+    private LocalDateTime taskEnd;
+
 }
